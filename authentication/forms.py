dc99657fb218869a2be45a0afbe81f9c447b1d3d
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-12 21:58:16 +0300

    formlar geldi

---
+++ authentication/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Project
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=40,label="Username",widget=forms.TextInput(attrs={
+        'placeholder':'username',
+        'autocomplete':'off'
+        }))
+
+    password1 = forms.CharField(max_length=40,label='Password',widget=forms.TextInput(attrs={
+        'placeholder':'password',
+        'autocomplete':'off'
+    }))
+
+    password2 = forms.CharField(max_length=40,label='Password Again',widget=forms.TextInput(attrs={
+        'placeholder':'password',
+        'autocomplete':'off',
+    }))
+
+    class Meta:
+        model = User
+        fields = {'username','password1','password2'}
+
+class project_form(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = {'link','notes','features'}
+        
+        labels = {
+            'link':'Link',
+            'notes':'Notes',
+            'features':'Features'
+        }
