0c8dfed81b0a4be9636fb4d300132d5aa0a041dd
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-12 21:44:47 +0300

    defining models without done_features

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    link = models.URLField()
+    notes = models.TextField()
+    features = models.CharField(max_length=160)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
+def login_view(request):
+    pass
+
+def register_view(request):
+    pass
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
