3eca73cc82ef154b301667a85fe5a639cf40f2b2
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-15 17:13:07 +0300

    ekleme devam

--- authentication/views.py
+++ authentication/views.py
@@ -34,3 +34,4 @@ def register_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
--- projects_shown/views.py
+++ projects_shown/views.py
@@ -55,13 +55,13 @@ def add_project(request):
 
 def add_feature(request, project_name,feature):
     project = get_object_or_404(Project, name=project_name)
-    Feature_form = feature_add()
+    feature_form = feature_add()
     
     if request.method == 'POST':
         feature_form = feature_add(request.POST)
         if feature_form.is_valid():
             feature_name = feature
-            new_feature = Feature_form.save(commit= False)
+            new_feature = feature_form.save(commit= False)
             new_feature.feature_add = feature_name
             new_feature.save()
             return redirect('projects', project_name=project_name)
