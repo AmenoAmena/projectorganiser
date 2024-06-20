9fda47a27762cd7adcc0c0f7feed1955c4a8db9d
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-20 08:52:28 +0300

    Yapıldı gibi şimdi denenecek

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class TokenField(models.CharField):
+    description = "A formatted text field with the pattern XXXX-XXXX-XXXX-XXXX"
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 19
+        super().__init__(*args, **kwargs)
+        self.validators.append(self.validate_format)
+
+    def validate_format(self, value):
+        if len(value) != 19:
+            raise ValidationError(f"{value} is not a valid token")
+        parts = value.split('-')
+        if len(parts) != 4:
+            raise ValidationError(f"{value} is not a valid token")
+        
+        for part in parts:
+            if len(part) != 4 or not part.isalpha() or not part.isupper():
+                raise ValidationError(f"{value} is not a valid token")    
 
 # Create your models here.
 class Feature(models.Model):
@@ -17,6 +37,7 @@ class Project(models.Model):
     features = models.ManyToManyField(Feature, blank=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     is_done = models.BooleanField(default=False)
+    token = TokenField()
 
     def __str__(self):
         return self.name
