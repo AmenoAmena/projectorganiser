a975c24557d33724bc386ed8d65ce5a9044d127a
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-20 09:08:55 +0300

    custom fields çalışıyor default olarak da koyuluyor

--- authentication/models.py
+++ authentication/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import random
 
 class TokenField(models.CharField):
     description = "A formatted text field with the pattern XXXX-XXXX-XXXX-XXXX"
 
+    
+    def generate_token(self):
+        letters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'
+        ]
+        result = ""
+        for i in range(0,16):
+            if i%4 == 0 and i != 0:
+                result += "-"
+            result += random.choice(letters)
+        return result 
+
+    
+
     def __init__(self, *args, **kwargs):
+        self.random_token = self.generate_token()
         kwargs['max_length'] = 19
-        kwargs['default'] = "XXXX-XXXX-XXXX-XXXX"
+        kwargs['default'] = self.random_token
         super().__init__(*args, **kwargs)
         self.validators.append(self.validate_format)
-
+    
+    
     def validate_format(self, value):
         if len(value) != 19:
             raise ValidationError(f"{value} is not a valid token")
@@ -20,8 +38,9 @@ class TokenField(models.CharField):
         
         for part in parts:
             if len(part) != 4 or not part.isalpha() or not part.isupper():
-                raise ValidationError(f"{value} is not a valid token")    
+                raise ValidationError(f"{value} is not a valid token")  
 
+    
 # Create your models here.
 class Feature(models.Model):
     feature_add = models.CharField(max_length=80)
