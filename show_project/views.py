bd567707c4eaf579e81139c40d2dc1683a6ec3b4
AmenoAmena <ardacavusoglu666@gmail.com>
2024-06-23 17:00:26 +0300

    form ile token gönderemiyorum ama tokenı urle yazarsam çalışyor

--- show_project/views.py
+++ show_project/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .forms import SeeForm
+from authentication.models import Project
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,10 @@ def index(request):
     return render(request, 'show_project/index.html',{
         'form':form
     })
+
+def project_room(request,project_token):
+    project = get_object_or_404(Project, token=project_token)
+    return render(request, 'show_project/project_room.html',{
+        'project':project
+    })
+    
